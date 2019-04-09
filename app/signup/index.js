dcc6376f816938350446373ff4699711a9edf2d8
Sam Liu <ontouchstart@gmail.com>
2019-04-09 11:54:03 -0400

    https://moca-spike-150.appspot.com/signup dump req.body as json

--- app/signup/index.js
+++ app/signup/index.js
@@ -3,7 +3,7 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport(config.mailer);
 
 const signup = (req, res, next) => {
-  const data = req.body.wpforms.fields;
+  const data = req.body;
   transporter.sendMail(config.mailer.mailOptions(data), (err, info) => {
     if(err) {
       console.log(err)
