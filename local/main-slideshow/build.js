d0d0128e91b08d9d54880363ad78cbaca45d2392
Sam Liu <ontouchstart@gmail.com>
2019-04-12 12:21:35 -0400

    self contained main-slideshow (need to be built under jekyll)

--- local/main-slideshow/build.js
+++ local/main-slideshow/build.js
@@ -3,28 +3,45 @@ const sharp = require('sharp')
 
 const w = 1920
 const h = 1080
-const build_path = '_includes/main-slideshow'
-const src_path = 'local/main-slideshow'
 
 const build = (fn) => {
-  const input = `${src_path}/${fn}.jpg`
+  const input = `${fn}.jpg`
   sharp(input)
     .resize({ width: w, height: h })
     .toBuffer()
     .then( (data) => { 
-       console.log('input', input)
        const html = `<img width="100%" src="data:image/png;base64,${data.toString('base64')}"/>`
-       const output = `${build_path}/${fn}.html`
-       fs.writeFile(output, html, (err) => {
-       if(err) {
-         console.log(err)
-	   }
-	   else {
-         console.log('output', output)
-	   }
-      })
+       const output = `${fn}.html`
+       fs.writeFile(output, html, () => {})
     });
 }
 
 build('01')
 build('02')
+
+Array.from(Array(5).keys()).forEach((i) => {
+  const delay = 10 + i * 5
+  const output = `${delay}s.html`
+  html = `---
+layout: plain
+---
+<html>
+<head>
+{% include_relative style.html %}
+</style>
+</head>
+<body>
+{% include_relative main.html %}
+</div>
+<script>
+const delay = ${delay * 1000};
+{% include_relative animation.html %}
+</script>
+</body>
+</html>
+`
+  fs.writeFile(output, html, () => {})
+
+  console.log(`<li><a href="${output}">${output}</a></li>`)
+})
+
