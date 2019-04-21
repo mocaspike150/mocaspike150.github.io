83f9f7a36e05cd18d82ff4e27abe1361ad3d9d2a
Sam Liu <ontouchstart@gmail.com>
2019-04-20 22:36:44 -0400

    https://www.mocaspike150.org/notebook/spike

---
+++ notebook/spike/624633383f012088.js
@@ -0,0 +1,28 @@
+// URL: https://observablehq.com/d/624633383f012088
+// Title: Untitled
+// Author: Sam Liu (@ontouchstart)
+// Version: 10
+// Runtime version: 1
+
+const m0 = {
+  id: "624633383f012088@10",
+  variables: [
+    {
+      inputs: ["html"],
+      value: (function(html){return(
+html`<svg width=100 height=200><g fill="#FBAC22">
+        <polygon points="16.109,0 16.109,117.862 16.135,117.862 21.641,164.522 41.076,117.862 41.076,0  "/>
+        <rect x="4.834" width="26.068" height="13.524"/>
+</g>
+</svg>`
+)})
+    }
+  ]
+};
+
+const notebook = {
+  id: "624633383f012088@10",
+  modules: [m0]
+};
+
+export default notebook;
