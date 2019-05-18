27316109fb0d42aa669d2a07003ba90c2079f5f8
Renny S <46349226+rennys66@users.noreply.github.com>
2019-05-17 21:48:44 -0400

    Update a9d47eda9c8eb67c.js

--- notebook/a9d47eda9c8eb67c/a9d47eda9c8eb67c.js
+++ notebook/a9d47eda9c8eb67c/a9d47eda9c8eb67c.js
@@ -1,11 +1,11 @@
 // URL: https://observablehq.com/d/a9d47eda9c8eb67c
 // Title: Untitled
 // Author: MOCA Spike 150 (@mocaspike150)
-// Version: 158
+// Version: 159
 // Runtime version: 1
 
 const m0 = {
-  id: "a9d47eda9c8eb67c@158",
+  id: "a9d47eda9c8eb67c@159",
   variables: [
     {
       inputs: ["html","club_leaderboard"],
@@ -431,7 +431,7 @@ html`${top50}`
     <table>
         <tr>
     <th>Rank</th>
-    <th>Club</th>
+    <!--<th>Club</th>-->
     <th>Name</th>
     <th>Distance</th>
   </tr>
@@ -442,7 +442,7 @@ html`${top50}`
       output +=  `
       <tr>
         <td>${counter+1} ${(counter < 3) ? '🏆' : ''}</td>
-        <td>${club.logo_html(line.club_id)}</td>
+       <!-- <td>${club.logo_html(line.club_id)}</td>-->
         <td>${line.name}</td>
         <td>${line.mile.toFixed(1)} miles</td>
       </tr>
@@ -497,7 +497,7 @@ d3.json("https://www.mocaspike150.org/api/leaderboard/week01/leaderboard.json")
 };
 
 const notebook = {
-  id: "a9d47eda9c8eb67c@158",
+  id: "a9d47eda9c8eb67c@159",
   modules: [m0]
 };
 
